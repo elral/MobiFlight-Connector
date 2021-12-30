ed9ae84c670a26c5172c0e7b557aea2d5f72fda5
elral <3263285+elral@users.noreply.github.com>
2021-12-30 15:05:08 +0100

    Changed Title

--- UI/MainForm.cs
+++ UI/MainForm.cs
@@ -1227,9 +1227,10 @@ namespace MobiFlight.UI
 
         private void SetTitle(string title)
         {
-            string NewTitle = "MobiFlight Connector ("+ Version +")";
-            if (VersionBeta.Split('.')[3]!="0") {
-                NewTitle = "MobiFlight Connector BETA (" + VersionBeta + ")";
+            string NewTitle = "MobiFlight Connector with ProMini/Teensy/BluePill and RaspiPico support (" + Version + ")";
+            if (VersionBeta.Split('.')[3] != "0")
+            {
+                NewTitle = "MobiFlight Connector with ProMini/Teensy/BluePill and RaspiPico support BETA (" + VersionBeta + ")";
             }
             if (title!=null && title!="")
             {
