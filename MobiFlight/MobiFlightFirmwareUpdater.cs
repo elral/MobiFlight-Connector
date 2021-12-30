faf307543eb106c45957fce619833bb8fdffa65f
elral <3263285+elral@users.noreply.github.com>
2021-12-30 14:57:04 +0100

    Teensy35/41 and Raspberry Pico support

--- MobiFlight/MobiFlightFirmwareUpdater.cs
+++ MobiFlight/MobiFlightFirmwareUpdater.cs
@@ -38,10 +38,18 @@ namespace MobiFlight
             String Port = module.InitUploadAndReturnUploadPort();
             if (module.Connected) module.Disconnect();
 
-            while (!SerialPort.GetPortNames().Contains(Port))
+            if (!(module.Board.Info.MobiFlightType == "MobiFlight RaspiPico"))
             {
-                System.Threading.Thread.Sleep(100);
+                while (!SerialPort.GetPortNames().Contains(Port))           // Don't do this for Raspberry Pico!! COM Port changes while in Bootloader and needs more time!
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
             }
+            else
+            {
+                System.Threading.Thread.Sleep(500);                         // wait for Bootloader to be ready for RaspiPico
+            }
+
 
             if (module.Board.AvrDudeSettings != null)
             {
@@ -65,13 +73,20 @@ namespace MobiFlight
 
         public static void RunAvrDude(String Port, Board board) 
         {
-            String FirmwareName = board.AvrDudeSettings.GetFirmwareName(board.Info.LatestFirmwareVersion); 
+            String FirmwareName = board.AvrDudeSettings.GetFirmwareName(board.Info.LatestFirmwareVersion);
+            if (board.Info.MobiFlightType == "MobiFlight RaspiPico")
+            {
+                FirmwareName = FirmwareName.Replace("hex", "elf");
+            } else if (board.Info.MobiFlightType == "MobiFlight BluePill")
+            {
+                FirmwareName = FirmwareName.Replace("hex", "elf");
+            }
             String ArduinoChip = board.AvrDudeSettings.Device;
             String Bytes = board.AvrDudeSettings.BaudRate;
             String C = board.AvrDudeSettings.Programmer;
             String message = "";
 
-            if (!IsValidFirmwareFilepath(FirmwarePath + "\\" + FirmwareName))
+             if (!IsValidFirmwareFilepath(FirmwarePath + "\\" + FirmwareName))
             {
                 message = "Firmware not found: " + FirmwarePath + "\\" + FirmwareName;
                 Log.Instance.log(message, LogSeverity.Error);
@@ -79,21 +94,60 @@ namespace MobiFlight
             }
 
             String verboseLevel = "";
-            
+
             //verboseLevel = " -v -v -v -v";
 
-            String FullAvrDudePath = $@"{ArduinoIdePath}\{AvrPath}";
-
             var proc1 = new ProcessStartInfo();
-            string anyCommand = 
-                $@"-C""{FullAvrDudePath}\etc\avrdude.conf"" {verboseLevel} -p{ArduinoChip} -c{C} -P\\.\{Port} -b{Bytes} -D -Uflash:w:""{FirmwarePath}\{FirmwareName}"":i";
-            proc1.UseShellExecute = true;
-            proc1.WorkingDirectory = $@"""{FullAvrDudePath}""";
-            proc1.FileName = $@"""{FullAvrDudePath}\bin\avrdude""";
-            proc1.Arguments = anyCommand;
-            proc1.WindowStyle = ProcessWindowStyle.Hidden;
-            Log.Instance.log("RunAvrDude : " + proc1.FileName, LogSeverity.Debug);
-            Log.Instance.log("RunAvrDude : " + anyCommand, LogSeverity.Debug);
+            string anyCommand = "";
+
+            if (board.Info.MobiFlightType == "MobiFlight RaspiPico")
+            {
+                String FullRaspiPicoUpdatePath = Directory.GetCurrentDirectory() + "\\RaspberryPico\\tools";
+                anyCommand = verboseLevel + "-v -D " + FirmwarePath + "\\" + FirmwareName;
+                proc1.WorkingDirectory = FullRaspiPicoUpdatePath;
+                proc1.FileName = "\"" + FullRaspiPicoUpdatePath + "\\rp2040load.exe" + "\"";
+                proc1.Arguments = anyCommand;
+                //proc1.WindowStyle = ProcessWindowStyle.Hidden;
+                Log.Instance.log("RunRaspberryPicoUpdater : " + proc1.FileName, LogSeverity.Info);
+                Log.Instance.log("RunRaspberryPicoUpdater : " + anyCommand, LogSeverity.Info);
+            }
+            else if (board.Info.MobiFlightType == "MobiFlight Teensy35" || board.Info.MobiFlightType == "MobiFlight Teensy41")
+            {
+                String FullTeensyUpdatePath = Directory.GetCurrentDirectory() + "\\Teensy\\tools";
+                anyCommand = verboseLevel + "-file=" + FirmwareName + " -path=" + "\"" + FirmwarePath + "\"" + " -tools=" + FullTeensyUpdatePath + " -board=" + ArduinoChip + " -reboot";
+                proc1.WorkingDirectory = FullTeensyUpdatePath;
+                proc1.FileName = "\"" + FullTeensyUpdatePath + "\\teensy_post_compile.exe" + "\"";
+                proc1.Arguments = anyCommand;
+                //proc1.WindowStyle = ProcessWindowStyle.Hidden;
+                Log.Instance.log("RunTeensyUpdater : " + proc1.FileName, LogSeverity.Info);
+                Log.Instance.log("RunTeensyUpdater : " + anyCommand, LogSeverity.Info);
+            } else if (board.Info.MobiFlightType == "MobiFlight BluePill")
+            {
+                // maple_upload COM33 2 1EAF:0003 "C:\_PlatformIO\MobiFlight-Firmware\.pio\build\bluepill\firmware.bin"
+                String FullBluePillUpdatePath = Directory.GetCurrentDirectory() + "\\STM32duino\\tools";
+                anyCommand = "/c maple_upload.bat " + verboseLevel + Port + " 2 1EAF:003 " + FirmwarePath + "\\" + FirmwareName;
+                proc1.WorkingDirectory = FullBluePillUpdatePath;
+                proc1.FileName = "\"" + FullBluePillUpdatePath + "\\cmd.exe" + "\"";
+                proc1.Arguments = anyCommand;
+                //    proc1.WindowStyle = ProcessWindowStyle.Hidden;
+                Log.Instance.log("RunBluePillUpdater : " + proc1.FileName, LogSeverity.Info);
+                Log.Instance.log("RunBluePillUpdater : " + anyCommand, LogSeverity.Info);
+
+                return;              // Flash Commands to be added
+            } else
+            {
+                String FullAvrDudePath = $@"{ArduinoIdePath}\{AvrPath}";
+
+                anyCommand =
+                    $@"-C""{FullAvrDudePath}\etc\avrdude.conf"" {verboseLevel} -p{ArduinoChip} -c{C} -P\\.\{Port} -b{Bytes} -D -Uflash:w:""{FirmwarePath}\{FirmwareName}"":i";
+                proc1.UseShellExecute = true;
+                proc1.WorkingDirectory = $@"""{FullAvrDudePath}""";
+                proc1.FileName = $@"""{FullAvrDudePath}\bin\avrdude""";
+                proc1.Arguments = anyCommand;
+                proc1.WindowStyle = ProcessWindowStyle.Hidden;
+                Log.Instance.log("RunAvrDude : " + proc1.FileName, LogSeverity.Debug);
+                Log.Instance.log("RunAvrDude : " + anyCommand, LogSeverity.Debug);
+            }
             Process p = Process.Start(proc1);
             if (p.WaitForExit(board.AvrDudeSettings.Timeout))
             {
